e43511e84bbe3c98bd7fd606e24e4b128372e397
vutnguyen <vu.tran.nguyen@c0x12c.com>
2025-04-16 16:53:57 +0700

    init left panel ui

---
+++ apps/web/client/src/app/project/[id]/_components/left-panel/layers-tab/tree/tree-row.tsx
@@ -0,0 +1,21 @@
+import type { LayerNode } from '@onlook/models/element';
+import type { RefObject } from 'react';
+import type { NodeApi } from 'react-arborist';
+
+interface TreeRowProps {
+    node: NodeApi<LayerNode>;
+    innerRef: RefObject<HTMLDivElement>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    attrs: Record<string, any>;
+    children: React.ReactNode;
+}
+
+const TreeRow = ({ innerRef, attrs, children }: TreeRowProps) => {
+    return (
+        <div ref={innerRef} {...attrs} className="outline-none">
+            {children}
+        </div>
+    );
+};
+
+export default TreeRow;
