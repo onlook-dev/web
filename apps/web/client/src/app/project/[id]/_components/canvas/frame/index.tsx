4039b34fec7357609d96c2df6ba06527de1aca13
Kiet Ho <hoakiet98@gmail.com>
2025-04-17 13:04:34 -0700

    more updates

--- apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
+++ apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
@@ -1,16 +1,17 @@
 import { FrameType, type Frame, type WebFrame } from "@onlook/models";
-import { observer } from "mobx-react-lite";
+import { useRef } from "react";
 import { GestureScreen } from './gesture';
 import { ResizeHandles } from './resize-handles';
 import { TopBar } from "./top-bar";
-import { WebFrameComponent } from "./web-frame";
+import { WebFrameComponent, type WebFrameView } from "./web-frame";
 
-export const FrameView = observer(
+export const FrameView =
     ({
         frame,
     }: {
         frame: Frame;
     }) => {
+        const webFrameRef = useRef<WebFrameView>(null);
         return (
             <div
                 className="flex flex-col fixed"
@@ -20,10 +21,10 @@ export const FrameView = observer(
                 </TopBar>
                 <div className="relative">
                     <ResizeHandles frame={frame} />
-                    {frame.type === FrameType.WEB && <WebFrameComponent frame={frame as WebFrame} />}
+                    {frame.type === FrameType.WEB && <WebFrameComponent frame={frame as WebFrame} ref={webFrameRef} />}
                     <GestureScreen frame={frame as WebFrame} />
                     {/* {domFailed && shouldShowDomFailed && renderNotRunning()} */}
                 </div>
             </div>
         );
-    });
+    };
