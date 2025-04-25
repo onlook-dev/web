facea1c83bec46aceecdbf313de8a03a55f8ff2c
Kiet Ho <hoakiet98@gmail.com>
2025-04-25 12:17:22 -0400

    update tests

--- packages/parser/src/packages.ts
+++ packages/parser/src/packages.ts
@@ -3,4 +3,3 @@ import { packages } from "@babel/standalone";
 export const { parse } = packages.parser;
 export const { generate } = packages.generator;
 export const traverse = packages.traverse.default;
-export const types = packages.types;
