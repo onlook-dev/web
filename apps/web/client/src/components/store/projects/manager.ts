66c6e9c9caa111ec71cef31c874c4d37cc331414
Kiet Ho <hoakiet98@gmail.com>
2025-05-14 10:46:48 -0700

    resume create

--- apps/web/client/src/components/store/projects/manager.ts
+++ apps/web/client/src/components/store/projects/manager.ts
@@ -1,9 +1,6 @@
 import { api } from '@/trpc/client';
-import { CSB_BLANK_TEMPLATE_ID } from '@onlook/constants';
-import type { Project as DbProject } from '@onlook/db';
 import type { Project } from '@onlook/models';
 import { makeAutoObservable, reaction } from 'mobx';
-import { v4 as uuidv4 } from 'uuid';
 import type { UserManager } from '../user/manager';
 
 export class ProjectsManager {
@@ -17,40 +14,6 @@ export class ProjectsManager {
             () => this.userManager.user?.id,
             () => this.fetchProjects(),
         );
-    }
-
-    async createProject() {
-        if (!this.userManager.user?.id) {
-            console.error('No user ID found');
-            return;
-        }
-        const { sandboxId, previewUrl } = await this.createSandbox();
-        const project = await this.createDefaultProject(sandboxId, previewUrl);
-        const newProject = await api.project.create.mutate({
-            project,
-            userId: this.userManager.user?.id,
-        });
-
-        return newProject;
-    }
-
-    createDefaultProject(sandboxId: string, previewUrl: string): DbProject {
-        const newProject = {
-            id: uuidv4(),
-            name: 'New project',
-            sandboxId,
-            sandboxUrl: previewUrl,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            previewImg: null,
-        } satisfies DbProject;
-        return newProject;
-    }
-
-    async createSandbox() {
-        return await api.sandbox.fork.mutate({
-            sandboxId: CSB_BLANK_TEMPLATE_ID,
-        });
     }
 
     async fetchProjects() {
